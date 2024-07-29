eb5b96c839cbfd5512877c39c304430e28628c9f
popuphudson <hudsondalton@outlook.com>
2024-07-29 17:18:28 +0100

    lighting kinda

--- Assets/Scripts/Managers/PowerManager.cs
+++ Assets/Scripts/Managers/PowerManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PowerManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] _lights;
     private bool _mapPowered;
 
     public void PowerMap() {
         _mapPowered = true;
+        foreach(GameObject light in _lights) {
+            light.SetActive(true);
+        }
     }
 
     public bool IsMapPowered() {
